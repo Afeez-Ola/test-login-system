6771e4b38fe5f92c3d6a0f6e357dbd410f14b0e0
Afeez-Ola <afeezbolajiola@gmail.com>
2021-06-24 23:56:13 +0100

    ✏️ TYPO:

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('This is the home page');
+    res.render('home');
 });
 
 module.exports = router;
