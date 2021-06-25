eaf28894923363f8099568251370c4fabae89f4f
Afeez-Ola <afeezbolajiola@gmail.com>
2021-06-25 11:44:43 +0100

    👌 UPDATE:

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../model/User');
+
 router.get('/login', (req, res) => {
     res.render('login');
 });
