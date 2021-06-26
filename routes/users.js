dceba524fb4b8d497bec95aae67fbcd80c2d3ccd
Afeez-Ola <afeezbolajiola@gmail.com>
2021-06-26 21:37:55 +0100

    🐛 BUG:

--- routes/users.js
+++ routes/users.js
@@ -91,5 +91,11 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
 });
 
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are now logged out!');
+    res.redirect('/users/login');
+});
+
 
 module.exports = router;
