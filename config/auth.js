fcc8df55696bffa1ba342b3110e704dba2510f31
Afeez-Ola <afeezbolajiola@gmail.com>
2021-06-28 14:54:26 +0100

    🚧 PROGRESS:

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,11 @@ module.exports = {
         if (req.isAuthenticated()) {
             return next();
         }
-        req.flash('error_msg', 'You need to log in to view this page');
         res.redirect('/users/login');
+        req.flash('error_msg', 'You need to log in to view this page');
+
+        req.session.returnTo = req.originalUrl;
+        res.redirect('/users/login');
+
     }
 }
