9e88bf220bb94be3ce41a3b7f68ce627be5030c8
Afeez-Ola <afeezbolajiola@gmail.com>
2021-07-02 18:26:20 +0100

    🔨 REFACTOR:

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,10 @@
 module.exports = {
     ensureAuthenticated: (req, res, next) => {
         if (req.isAuthenticated()) {
+            res.redirect('/dashboard');
+
             return next()
+
         } else {
             req.flash('error_msg', 'You need to log in to view this page');
             res.redirect('/users/login');
